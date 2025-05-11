7c36210c1f7e2696385e7e1ac5505917840937ac
Daniel Trzeszczkowski <daniel_trzeszczkowski@o2.pl.>
2025-05-11 18:45:51 +0200

    Pierwsze repo react

---
+++ kodilla-react/src/components/List/List.js
@@ -0,0 +1,24 @@
+const List = () => {
+    return (
+     <div>
+      <header>Things to do<span>soon</span>
+        <p>Interesting things I want to check out</p>
+      </header>
+        <section>
+            <article>
+                <h1>Books</h1>
+            </article>
+            <article>
+                <h1>Movies</h1>
+            </article>
+            <article>
+                <h1>Games</h1>
+            </article>
+        </section>
+     </div>  
+      
+      
+    );
+  };
+
+  export default List;
